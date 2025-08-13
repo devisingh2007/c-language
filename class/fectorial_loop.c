c1da5f9fc2e4962100bd7cecc5824c87e2177892
devisingh rajput <devisinghrajput.cg@gmail.com>
2025-08-13 16:00:55 +0530

    13/08/2025 class

---
+++ class/fectorial_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+
+{
+    int a, t = 1;
+    printf("enter the number which one you need fectorial :");
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++)
+    {
+        t = t * i;
+    }
+    printf("%d", t);
+}
