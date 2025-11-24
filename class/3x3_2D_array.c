c15323e3928d3d3f02634fbe76e8bc4b000499f4
devisingh rajput <devisinghrajput.cg@gmail.com>
2025-11-24 11:30:54 +0530

    2D Array syntax

---
+++ class/3x3_2D_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int arr[3][3]={
+        {0,1,2},
+        {3,4,5},
+        {6,7,8}
+    };
+/*
+    printf("%d\n",arr[0][2]);
+    printf("%d\n",arr[2][2]);
+    printf("%d\n",arr[2][0]);
+    printf("%d\n",arr[1][0]);
+    printf("%d\n",arr[1][2]);
+*/
+ int arr2[2][4]={
+        {1,2,3,4},
+        {5,6,7,8}
+    };
+ printf("%d\n",arr2[0][2]);
+    printf("%d\n",arr2[1][3]);
+    printf("%d\n",arr2[1][1]);
+    return 0;
+}
